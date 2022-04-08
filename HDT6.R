8c25c3fbf51d493b8d57522fc66b58ac36eebaec
coutino568 <40869460+coutino568@users.noreply.github.com>
2022-04-07 23:43:11 -0600

    Definicion de grupos
    Grupos de Testing y training

--- HDT6.R
+++ HDT6.R
@@ -1,10 +1,11 @@
 getwd()
-setwd("D:/UVG/2022/Semestre 1 2022/Mineria de datos/HDT5")
+setwd("D:/UVG/2022/Semestre 1 2022/Mineria de datos/HDT6")
 df_test <- read.csv("test.csv")
 df_train<- read.csv("train.csv")
 df_test2 <- read.csv("sample_submission.csv")
+df_test['SalePrice']<-df_test2$SalePrice
 
-df_
+set.seed(123)
 library(ggplot2)
 library (dplyr)
 library(naivebayes)
@@ -19,42 +20,31 @@ library(klaR)
 summary(df_train$SalePrice)
 priceRange <- max(df_train$SalePrice)-min(df_train$SalePrice)
 baratoMax<- min(df_train$SalePrice)+(priceRange/3)
-medianoMax <- baratomax+(priceRange/3)
+medianoMax <- baratoMax+(priceRange/3)
 caroMax<-max(df_train$SalePrice)
-max(df_train$SalePrice)
+min(df_train$SalePrice)
+max(df_test$SalePrice)
 (medianoMax)
 baratoMax
-df_train['tipoDeCasa']<- ifelse(df_train$SalePrice<baratoMax,"BARATA",ifelse(df_train$SalePrice>=baratoMax & df_train$SalePrice<medianoMax,"MEDIA","CARA"))
-df_train_filtered<-df_train[,c(2,19,20,35,45,48,52,71,82)]
-df_train_filtered<-df_train[,c(81,82)]
-## test no tiene saleprice entonces debemos unirlo con sample submission.csv
-df_test['SalePrice']<-df_test2$SalePrice
-df_test['tipoDeCasa']<-ifelse(df_test2$SalePrice<baratoMax,"BARATA",ifelse(df_test2$SalePrice>=baratoMax & df_test2$SalePrice<medianoMax,"MEDIA","CARA"))
-df_test_filtered<-df_test[,c(2,19,20,35,45,48,52,71,82)]
 
+## Definicion de variables dicotomicas trainig dataset
+df_train['esBarata']<- ifelse(df_train$SalePrice<baratoMax,1,ifelse(df_train$SalePrice>=baratoMax & df_train$SalePrice<medianoMax,0,0))
+df_train['esMediana']<- ifelse(df_train$SalePrice<baratoMax,0,ifelse(df_train$SalePrice>=baratoMax & df_train$SalePrice<medianoMax,1,0))
+df_train['esCara']<- ifelse(df_train$SalePrice<baratoMax,0,ifelse(df_train$SalePrice>=baratoMax & df_train$SalePrice<medianoMax,0,1))
 
+## Definicion de variables dicotomicas testing dataset
+df_test['esBarata']<- ifelse(df_test$SalePrice<baratoMax,1,ifelse(df_test$SalePrice>=baratoMax & df_test$SalePrice<medianoMax,0,0))
+df_test['esMediana']<- ifelse(df_test$SalePrice<baratoMax,0,ifelse(df_test$SalePrice>=baratoMax & df_test$SalePrice<medianoMax,1,0))
+df_test['esCara']<- ifelse(df_test$SalePrice<baratoMax,0,ifelse(df_test$SalePrice>=baratoMax & df_test$SalePrice<medianoMax,0,1))
 
-## parte de prueba 
-df_train_filtered<-df_train[,c(81,82)]
-df_train_filtered$tipoDeCasa <-df_train_filtered$tipoDeCasa
+## comentario, en el set de testing el valor maximo es menor a medianoMax (que es el valor a partir del cual las casas son caras. en testing no habran casas caras.)
 
-df_test_filtered<-df_test[,c(81,82)]
-df_test_filtered$tipoDeCasa <-df_test_filtered$tipoDeCasa
+trainSliced <- df_train[,c(81,82,83,84)]
+trainSliced
+##REgresion logistica
 
+modelo<- glm(casasCaras~. , data=trainSliced, family= binomial(), maxit=100)
 
-
-modelo<-naiveBayes(tipoDeCasa~., data=df_train_filtered)
-predBayes<-predict(modelo, newdata = df_test_filtered[,c(1)])
-
-
-
-cm<-caret::confusionMatrix(predBayes, df_test_filtered$tipoDeCasa)
-predBayes
-
-
-
-
-
-
-levels(df_test_filtered$tipoDeCasa)<-levels(predBayes)
-df_test_filtered$tipoDeCasa
+pred<-predict(modelo,newdata = test[,1:4], type = "response")
+prediccion<-ifelse(pred>=0.5,1,0)
+confusionMatrix(as.factor(test$datosvirginica),as.factor(prediccion))
